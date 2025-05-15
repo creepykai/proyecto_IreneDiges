c23690c57bacb79de2712cd309e7b3d5203efed6
creepykai <raleydg@gmail.com>
2025-05-15 14:50:21 +0200

    15/05/2025

--- controladores/gestionar_alumnos.py
+++ controladores/gestionar_alumnos.py
@@ -5,9 +5,28 @@ def crear_alumno() -> Alumno:
     nombre : str = ""
     apellido : str = ""
     tramo : str = ""
-    bilingue_respuesta : str = ""
+    bilingue_str : str = ""
+    bilingue : bool = False
 
     nie : str = input("Ingrese el NIE del alumno (00000000L): ").strip().upper()
     nombre : str = input("Ingrese el nombre del alumno: ").strip()
     apellido : str = input("Ingrese el apellido del alumno: ").strip()
     tramo : str = input("Ingrese el tramo (0,I,II): ").strip().upper()
+
+    bilingue_str= input("¿Es bilingüe? (s/n): ").strip().lower()
+
+    if bilingue_str == "s":
+        bilingue = True
+    elif bilingue_str == "n":
+        bilingue = False
+    else:
+        print("Entrada no válida para bilingüe. Se ha tomado como 'No bilingüe'")
+
+    alumno: Alumno = Alumno(nombre, apellido, tramo, bilingue)
+
+    if alumno.validacion_datos_alumno():
+        print("El alumno se ha creado correctamente.")
+        return alumno
+    else:
+        print("No se ha podido crear el alumno.")
+        return None
