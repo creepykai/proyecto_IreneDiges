ac8e45e1efa01efe3592c9031bb1946b0e5370e1
creepykai <raleydg@gmail.com>
2025-05-15 13:55:49 +0200

    15/05/2025

--- controladores/gestionar_alumnos.py
+++ controladores/gestionar_alumnos.py
@@ -5,6 +5,9 @@ def crear_alumno() -> Alumno:
     nombre : str = ""
     apellido : str = ""
     tramo : str = ""
+    bilingue_respuesta : str = ""
 
     nie : str = input("Ingrese el NIE del alumno (00000000L): ").strip().upper()
-
+    nombre : str = input("Ingrese el nombre del alumno: ").strip()
+    apellido : str = input("Ingrese el apellido del alumno: ").strip()
+    tramo : str = input("Ingrese el tramo (0,I,II): ").strip().upper()
